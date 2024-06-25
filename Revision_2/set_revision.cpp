c72f006c648e308b69163ac092d2adf8cccc9d95
ozaharsh95 <dsouzaaron77@gmail.com>
2024-06-25 23:28:50 +0530

    25june24

---
+++ Revision_2/set_revision.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    //  set 
+    set<int> s1;
+    
+    // to insert element
+    s1.insert(100);
+    s1.insert(24);
+    s1.insert(35);
+    s1.insert(50);
+    
+    
+    // to print set
+    for(auto it : s1){
+        cout<<it<<" ";
+    }
+    cout<<endl;
+    //or
+    for(auto it=s1.begin();it!=s1.end();it++){
+        cout<<*it<<" ";
+    }
+    cout<<endl;
+    
+    // find element
+    auto it = s1.find(24);
+    
+    if(it!=s1.end()){
+        cout<<"Element is found : "<<*it<<endl;
+    }else{
+        cout<<"Element is not found !!!"<<endl;
+    }
+    
+    // size of set
+    cout<<"Size of the set : "<<s1.size()<<endl;
+    
+    // to count frequency of Element
+    cout<<s1.count(24)<<endl;
+    
+    
+    // unordered set
+    unordered_set<int> uns1;
+    
+    uns1.insert(100);
+    uns1.insert(101);
+    uns1.insert(50);
+    uns1.insert(25);
+    uns1.insert(2000);
+    
+    for(auto it:uns1){
+        cout<<it<<" ";
+    }
+    cout<<endl;
+    
+    // find element
+    auto it1 = uns1.find(101);
+    
+    if(it1!=uns1.end()){
+        cout<<"Element is found : "<<*it1<<endl;
+    }else{
+        cout<<"Element is not found !!!"<<endl;
+    }
+    
+    return 0;
+}
+
+/*
+
+24 35 50 100 
+24 35 50 100 
+Element is found : 24
+Size of the set : 4
+1
+25 2000 50 101 100 
+Element is found : 101
+
+*/
